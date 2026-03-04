f45929afad204ec5eee64304aa48b5b130f66b77
MD. ASHIK IKBAL <ikbalmdashikk@gmail.com>
2026-03-05 00:42:16 +0600

    Fix Minor Animation Bug

--- frontend-nextjs/app/pages/chat/page.tsx
+++ frontend-nextjs/app/pages/chat/page.tsx
@@ -1,16 +1,24 @@
-import ChatComponent from "@/app/components/chat/chat";
-import { Metadata } from "next";
+"use client"
 
-export const metadata: Metadata = {
-    title: "Chats"
-}
+import { Spinner } from "@/components/ui/spinner";
+import dynamic from "next/dynamic";
 
 const Chat = () => {
-    return (
-        <>
-            <ChatComponent />
-        </>
+    const ChatComponent = dynamic(
+        () => import("@/app/components/chat/chat"),
+        {
+            loading: () => {
+                return (
+                    <div className="bg-white dark:bg-slate-950 flex items-center justify-center min-h-screen">
+                        <Spinner size={50} />
+                    </div>
+                );
+            },
+            ssr: false,
+        }
+
     );
-}
+    return <ChatComponent />;
+};
 
 export default Chat;
