9da0e7abe4b6f5647c38cee60a2d4d501f6802fa
Md Ashik Ikbal <ikbalmdashikk@gmail.com>
2026-09-12 06:35:46 +0600

    Updated Prod Config

--- backend-nestjs/src/mailer/mail.service.ts
+++ backend-nestjs/src/mailer/mail.service.ts
@@ -35,11 +35,13 @@ export class MailService {
         this.transporter = nodemailer.createTransport({
             host: this.configService.get('EMAIL_SERVICE'),
             port: this.configService.get('EMAIL_PORT'),
+            secure: false,
             // service: this.configService.get('EMAIL_SERVICE'),
             auth: {
                 user: this.configService.get('EMAIL_USER'),
                 pass: this.configService.get('EMAIL_PASSWORD'),
             },
+            family: 4
         });
     }
 
